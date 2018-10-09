ae5642d66a125cdb3225f567e46d47449432da53
Saulo T. Cavalieri <tk.saulo@gmail.com>
2018-10-09 17:41:56 -0300

    Card tests

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
 var game = document.getElementById("memory-game");
+var cardSize = 100;
+var cardSpacing = 10;
 
 createCards();
+
 //Create the default card
 function createCards() {
   var card = document.createElement("img");
+
   card.src = "./assets/img/back.png";
   card.style.position = "absolute";
-  card.style.left = "100px";
-  card.style.top = "100px";
+  card.style.left = 1 * (cardSize + cardSpacing) + cardSpacing + "px";
+  card.style.top = 1 * (cardSize + cardSpacing) + cardSpacing + "px";
   game.appendChild(card);
 }
