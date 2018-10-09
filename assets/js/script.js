bcd51f5942c17816dcc0d8ba8a4d579bac366961
Saulo T. Cavalieri <tk.saulo@gmail.com>
2018-10-09 17:47:28 -0300

    Manipulating data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,25 @@
 var game = document.getElementById("memory-game");
 var cardSize = 100;
 var cardSpacing = 10;
+var columns = 4;
+var lines = 4;
 
-createCards();
+createGame();
+function createGame() {
+  for (var x = 0; x < columns; x++) {
+    for (var y = 0; y < lines; y++) {
+      createCards(x);
+    }
+  }
+}
 
 //Create the default card
-function createCards() {
+function createCards(x) {
   var card = document.createElement("img");
 
   card.src = "./assets/img/back.png";
   card.style.position = "absolute";
-  card.style.left = 1 * (cardSize + cardSpacing) + cardSpacing + "px";
-  card.style.top = 1 * (cardSize + cardSpacing) + cardSpacing + "px";
+  card.style.left = x * (cardSize + cardSpacing) + cardSpacing + "px";
+  card.style.top = x * (cardSize + cardSpacing) + cardSpacing + "px";
   game.appendChild(card);
 }
