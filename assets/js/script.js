9af5a5196a32a57546b70c94b4912c3b9d45fcf7
Saulo T. Cavalieri <tk.saulo@gmail.com>
2018-10-09 18:56:23 -0300

    Test click card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,38 @@
 var game = document.getElementById("memory-game");
+
 var cardSize = 100;
 var cardSpacing = 10;
 var columns = 4;
 var lines = 4;
+var firstOption = null;
+var secondOption = null;
 
-createGame();
-function createGame() {
+createGame(columns, lines);
+
+function createGame(columns, lines) {
+  //Generate with my shuffle vector
   for (var x = 0; x < columns; x++) {
     for (var y = 0; y < lines; y++) {
-      createCards(x);
+      createCards(0, x, y);
     }
   }
 }
 
 //Create the default card
-function createCards(x) {
+function createCards(cardNum, posX, posY) {
   var card = document.createElement("img");
-
+  card.num = cardNum;
   card.src = "./assets/img/back.png";
   card.style.position = "absolute";
-  card.style.left = x * (cardSize + cardSpacing) + cardSpacing + "px";
-  card.style.top = x * (cardSize + cardSpacing) + cardSpacing + "px";
+  card.style.left = 10 + posX * (cardSize + cardSpacing) + 8 + "px";
+  card.style.top = 15 + posY * (cardSize + cardSpacing) + 75 + "px";
+  card.onclick = clickedCard;
   game.appendChild(card);
+}
+
+//Selected card by the user
+function clickedCard(e) {
+  var clicked = e.target;
+
+  alert(clicked.num);
 }
